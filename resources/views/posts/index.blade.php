bdb90f376759c6b4e6e8f677b64367390d86cbcd
Revi Permana <repikerja2@gmail.com>
2024-01-26 17:28:23 +0700

    Menginstall  bootstrap ke dalam laravel

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- css bootstrap --}}
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    {{-- js bootstrap --}}
+    <script src="{{  asset('bootstrap-5/js/bootstrap.min.js') }}" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>Blog</title>
     <style>
         .blog {
@@ -16,11 +20,8 @@
     </style>
 </head>
 <body>
-    <h1>Blog Widyatama</h1>
-
-
-
-    <div>
+    <div class="container">
+        <h1>Blog Widyatama</h1>
         @php($number = 1)
         @foreach ($posts as $post)
         <div class="blog">
